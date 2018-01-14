87877485852f608457a91f51922960282a12271c
Yonatan Girma <ygirma.exclusive@yahoo.com>
2018-01-13 21:25:32 -0600

    Deque implementaion using Python

---
+++ deque.py
@@ -0,0 +1,24 @@
+#Implementation of Deque in python
+
+class Deque(object):
+
+    def __init__(self):
+        self.items = []
+
+    def addFront(self,item):
+        self.items.insert(0,item)
+
+    def addRear(self,item):
+        self.items.append(item)
+
+    def removeFront(self):
+        return self.items.pop()
+
+    def removeRear(self):
+        return self.items.pop(0)
+
+    def isEmpty(self):
+        return self.items == []
+
+    def size(self):
+        return len(self.items)
